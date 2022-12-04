26a019b0eef949c1f235e45cc906d6fd51a8fe5c
coomputer1426 <54347238+coomputer1426@users.noreply.github.com>
2022-12-04 21:34:57 +0300

    + Contacts Assignment textfield class

--- islami2/lib/statefulwidgcontactsassign2p1/mycustomform.dart
+++ islami2/lib/statefulwidgcontactsassign2p1/mycustomform.dart
@@ -12,17 +12,38 @@ class _MyCustomFormState extends State<MyCustomForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(key: _formkey ,child: Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[TextFormField(validator: (String? value){
+    return Container(color: Color(0xFF9E9E9E),
+      child: Form(key: _formkey ,child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          SizedBox(height: 50,width: double.infinity,),
+          ContactTextField("Name"),
+          SizedBox(height: 20,width: double.infinity,),
+          ContactTextField("Phone Number"),
+          SizedBox(height: 20,width: double.infinity,),
+
+          Padding(padding: EdgeInsets.symmetric(vertical: 16),child:
+            ElevatedButton(onPressed: (){if(_formkey.currentState!.validate())
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Processing Data")),
+              );},child: Text("Submit"),),)
+        ],
+        )),
+    );
+  }
+}
+class ContactTextField extends StatelessWidget {
+  // const ContactTextField({Key? key}) : super(key: key);
+  String type;
+  ContactTextField(this.type);
+  @override
+  Widget build(BuildContext context) {
+    return Container(width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.white),
+      margin: EdgeInsets.symmetric(horizontal: 15),
+      child: TextFormField(validator: (String? value){
         if(value==null || value.isEmpty){
           return "Please Enter some text";
-        }return null;},decoration: InputDecoration(hintText: "Enter"), ),
-        Padding(padding: EdgeInsets.symmetric(vertical: 16),child:
-          ElevatedButton(onPressed: (){if(_formkey.currentState!.validate())
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Processing Data")),
-            );},child: Text("Submit"),),)
-      ],
-      ));
+        }return null;},decoration: InputDecoration(hintText: "Enter Your $type"), ),
+    );
   }
 }
